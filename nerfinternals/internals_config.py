eaabb8de36c471941326eff6c2c89f6c430ef53c
Lukas Radl <radl-lukas@gmx.at>
2023-05-23 20:21:54 +0200

    final changes to mipnerf for today

--- nerfinternals/internals_config.py
+++ nerfinternals/internals_config.py
@@ -48,17 +48,19 @@ activation_mipnerf = MethodSpecification(
             datamanager=VanillaDataManagerConfig(
                 dataparser=BlenderDataParserConfig(),
             ),
-            model=NeRFInternalModelConfig(_target=MipNerfInternalModel),
+            model=NeRFInternalModelConfig(
+                _target=MipNerfInternalModel,
+                loss_coefficients={"rgb_loss_coarse": 0.1, "rgb_loss_fine": 1.0},
+                num_coarse_samples=128,
+                num_importance_samples=128,
+                eval_num_rays_per_chunk=1024,
+            ),
         ),
         optimizers={
             "fields": {
                 "optimizer": RAdamOptimizerConfig(lr=5e-4, eps=1e-08),
                 "scheduler": None,
-            },
-            "temporal_distortion": {
-                "optimizer": RAdamOptimizerConfig(lr=5e-4, eps=1e-08),
-                "scheduler": None,
-            },
+            }
         },
         vis="tensorboard",
     ),
