a787df3c507cc2d0bc40b86b54ee8593fea26b44
Lukas Radl <radl-lukas@gmx.at>
2023-05-26 12:27:22 +0200

    webpage test

--- nerfinternals/internals_dataparser_config.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from nerfstudio.plugins.registry_dataparser import DataParserSpecification
-from nerfinternals.llff_dataparser import LLFFDataParserConfig
-
-llff_data = DataParserSpecification(config=LLFFDataParserConfig())
